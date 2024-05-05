ff25555e6b57b24a05720a7962ea14833efb64c0
Prince Kumar <prince.kumar@goniyo.com>
2024-05-05 22:59:48 +0530

    link updated

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -54,7 +54,7 @@ export const InfoModal = ({ isOpen, handleClose, darkMode, styles }: Props) => (
           {/* <span>This project is open source on</span> */}
           <a
             className="ml-[6px] rounded-full h-5 w-5 sm:h-6 sm:w-6"
-            href="https://github.com/octokatherine/word-master"
+            href="https://prince-kumar.github.io/word-master/"
             target="_blank"
             rel="noreferrer"
           >
